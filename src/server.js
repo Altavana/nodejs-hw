9db4527e42ebdea223fafb49f5a014355b46ea8b
Anna D <altavanamail@gmail.com>
2026-03-17 01:27:44 +0200

    running server

---
+++ src/server.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import 'dotenv/config';
+
+const app = express();
+// Використовуємо значення з .env або дефолтний порт 3000
+const PORT = process.env.PORT ?? 3000;
+
+// Middleware для парсингу JSON
+app.use(express.json());
+app.use(cors()); // Дозволяє запити з будь-яких джерел
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+// Список усіх  нотаток
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+// повертати одну нотатку за її ідентифікатором id
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
+
+// Маршрут для тестування middleware помилки
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+// Middleware 404 (після всіх маршрутів)
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+// Middleware для обробки помилок
+app.use((err, req, res, next) => {
+  console.error('Error:', err.message);
+  res.status(500).json({ message: err.message });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
